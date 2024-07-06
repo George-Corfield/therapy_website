6ea52308ab1f14510129f94c86ccaef046e433ee
georgecorfield <gacorfield50@gmail.com>
2024-07-06 23:09:49 +0100

    Making Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Navbar />
+      <div className="content">
+        <h2>Content</h2>
+      </div>
     </div>
   );
 }
