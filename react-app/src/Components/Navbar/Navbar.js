f3d5783b5e0152fe1a042e13e98bdb05cc716d26
georgecorfield <gacorfield50@gmail.com>
2024-07-06 23:09:59 +0100

    Making Components

---
+++ react-app/src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Vicky Corfield Counselling</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">about</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
